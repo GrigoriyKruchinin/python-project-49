408c3660b4d60c9f6fe39b6bfc3121447b35bddc
GrigoriyKruchinin <gkruchinin75@gmail.com>
2023-05-19 08:03:28 +0200

    linter correction

--- brain_games/logic_even.py
+++ brain_games/logic_even.py
@@ -13,11 +13,13 @@ def play_game(name):
         print(f"Question: {number}")
         user_answer = input("Your answer: ")
 
-        if (user_answer == "yes" and is_even(number)) or (user_answer == "no" and not is_even(number)):
+        if (user_answer == "yes" and is_even(number)) or \
+                (user_answer == "no" and not is_even(number)):
             print("Correct!")
             correct_answers += 1
         else:
-            print(f"'{user_answer}' is the wrong answer ;(. Correct answer was '{'yes' if is_even(number) else 'no'}'.")
+            print(f"'{user_answer}' is the wrong answer ;(. "
+                  f"Correct answer was '{'yes' if is_even(number) else 'no'}'.")
             print("Let's try again!")
             return
 
